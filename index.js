11ea7dcca2d39d51c4936367eae58368ae537428
cescoferraro <francescoaferraro@gmail.com>
2017-03-16 14:33:16 -0300

    nsdkfj

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var MyDebugger = require("debug");
+console.warn("sdfj")
+
+var react = require("react");
+var react = require("react");
+var react = require("react");
+var react = require("react");
+var react = require("react");
+var react = require("react");
+
+console.log("fdgdf")
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/fatih/color"
 	"log"
 )
 
 func main() {
-	log.Panicln("dsfmsldkf")
-	log.Printf("sdjnjfk")
+	color.Cyan("Prints text in cyan.")
+	log.Println("dsfmsldkf")
 }

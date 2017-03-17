04597b17e1424cc764ea03f1020c6f8f0b92ac1c
cesco <francescoaferraro@gmail.com>
2017-03-17 12:07:56 -0300

    Update main.sh

--- src/os/install/main.sh
+++ src/os/install/main.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "../utils.sh"
+&& . "../utils.sh"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 "./$(get_os)/main.sh"
-./nvm.sh
-./gvm.sh
-./npm.sh
-./vim.sh
+
+
+
+
+print_in_purple "\n • Golang\n\n"
+
+ask_for_confirmation "Do you want to set Golang environment?"
+if answer_is_yes; then
+    ./gvm.sh
+fi
+
+
+
+print_in_purple "\n • Node.js\n\n"
+
+ask_for_confirmation "Do you want to set Node.js environment?"
+if answer_is_yes; then
+    ./nvm.sh
+    ./npm.sh
+fi
+
+print_in_purple "\n • Vim\n\n"
+
+ask_for_confirmation "Do you want to set vim environment?"
+if answer_is_yes; then
+    ./vim.sh
+fi
+

a525ce8e62022008b604d4a75170c070ab512357
cesco <francescoaferraro@gmail.com>
2017-03-17 19:49:22 -0300

    Update main.sh

--- src/os/install/main.sh
+++ src/os/install/main.sh
@@ -5,10 +5,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-"./$(get_os)/main.sh"
 
 
 
+print_in_purple "\n • OS\n\n"
+
+ask_for_confirmation "Do you want to set OS environment?"
+if answer_is_yes; then
+    "./$(get_os)/main.sh"
+fi
 
 print_in_purple "\n • Golang\n\n"
 

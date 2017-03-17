c2499fa2c7fb238df8219a39274f78c8cf94f1b9
cesco <francescoaferraro@gmail.com>
2017-03-17 19:52:22 -0300

    Update main.sh

--- src/os/preferences/main.sh
+++ src/os/preferences/main.sh
@@ -7,4 +7,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n • Preferences\n"
 
-"./$(get_os)/main.sh"
+
+ask_for_confirmation "Do you want to set OS Preferences?"
+if answer_is_yes; then
+    "./$(get_os)/main.sh"
+fi

7aad6260f87450da1cf6a358eda1ad62262ed95f
cescoferraro <francescoaferraro@gmail.com>
2017-03-23 19:18:20 -0300

    add docker package + neotreee shortcut

--- src/emacs.d/lisp/neotree.el
+++ src/emacs.d/lisp/neotree.el
@@ -3,6 +3,14 @@
 
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
+
+(add-hook 'neotree-mode-hook
+    (lambda ()
+        (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+        (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+        (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

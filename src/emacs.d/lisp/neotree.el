096a0c70b6ef020394bbbbf9490e9ad8a5654c78
cescoferraro <francescoaferraro@gmail.com>
2017-03-25 20:46:22 -0300

    Neotree arrow fixes. Add Markdown mode.

--- src/emacs.d/lisp/neotree.el
+++ src/emacs.d/lisp/neotree.el
@@ -1,22 +1,28 @@
 (require 'neotree)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
-    (evil-leader/set-key
-    "m"  'neotree-toggle
-    "n"  'neotree-project-dir)
+(evil-leader/set-key
+"m"  'neotree-toggle
+"n"  'neotree-project-dir)
 
-  (setq projectile-switch-project-action 'neotree-projectile-action)
-  (add-hook 'neotree-mode-hook
-    (lambda ()
-      (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-      (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
-      (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
-      (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
-      (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
-      (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
-      (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
+(setq projectile-switch-project-action 'neotree-projectile-action)
 
-      (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
-      (define-key evil-normal-state-local-map (kbd "S") 'neotree-enter-horizontal-split)
+(add-hook 'neotree-mode-hook
+  (lambda ()
+    (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+    (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
+    (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
+    (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
+    (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
+    (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
+    (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
 
-      (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+    (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
+    (define-key evil-normal-state-local-map (kbd "S") 'neotree-enter-horizontal-split)
+
+    (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+    (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+    )
+  )
+  
 (provide 'init-neotree)

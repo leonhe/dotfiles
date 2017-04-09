806350f1efa58e000a59d9cda2d8f9eab518318c
cescoferraro <francescoaferraro@gmail.com>
2017-04-09 17:53:21 -0300

    Movinf foward

--- src/emacs.d/custom.el
+++ src/emacs.d/custom.el
@@ -5,9 +5,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+    '(custom-safe-themes
+         (quote
+             ("f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
     '(package-selected-packages
          (quote
-             (iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window))))
+             (evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

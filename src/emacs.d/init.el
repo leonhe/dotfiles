4f8bfce73c6a1bf4d7a480470bea5b5f44356bdc
cescoferraro <francescoaferraro@gmail.com>
2017-04-23 05:26:34 -0300

    zamansky copied snippets! + custom redux snippets + stuff

--- src/emacs.d/init.el
+++ src/emacs.d/init.el
@@ -1,17 +1,8 @@
-;; Setup package-manager
+
 (package-initialize)
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
-(require 'pallet)
-(pallet-mode t)
 
-;; load use-package
-(eval-when-compile (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
-
-
-;; Custom configuration
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 

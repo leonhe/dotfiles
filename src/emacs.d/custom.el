b5d4275f219ac9b2ced3df1dd92689269cdd1709
cescoferraro <francescoaferraro@gmail.com>
2017-04-26 16:39:46 -0300

    eclipse settings! working on the article

--- src/emacs.d/custom.el
+++ src/emacs.d/custom.el
@@ -9,11 +9,17 @@
  '(inhibit-startup-screen t)
     '(package-selected-packages
          (quote
-             (interleave w3m find-file-in-project helm-ag tabbar-ruler tabbar magit-gitflow company-go expand-line evil-nerd-commenter evil-commentary helm-dash timonier evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window)))
+             (company-emacs-eclim eclim interleave w3m find-file-in-project helm-ag tabbar-ruler tabbar magit-gitflow company-go expand-line evil-nerd-commenter evil-commentary helm-dash timonier evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window)))
  '(tabbar-separator (quote (0.5))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(tabbar-button ((t (:inherit tabbar-default :foreground "#f2f0ec"))))
+ '(tabbar-button-highlight ((t (:inherit tabbar-default))))
+ '(tabbar-default ((t (:inherit variable-pitch :background "#393939" :foreground "black" :weight bold))))
+ '(tabbar-highlight ((t (:underline t))))
+ '(tabbar-selected ((t (:inherit tabbar-default :background "#393939" :foreground "#ffcc66"))))
+ '(tabbar-separator ((t (:inherit tabbar-default :background "#393939"))))
+ '(tabbar-unselected ((t (:inherit tabbar-default)))))

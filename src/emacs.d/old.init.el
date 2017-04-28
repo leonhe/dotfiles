565025cb2fb234c035b06c4a359047986bc07887
cescoferraro <francescoaferraro@gmail.com>
2017-04-28 02:24:14 -0300

    eclim

--- src/emacs.d/old.init.el
+++ src/emacs.d/old.init.el
@@ -30,3 +30,41 @@
 
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
+   #+BEGIN_SRC emacs-lisp
+
+     (require 'meghanada)
+     (require 'flycheck)
+     (require 'evil-leader)
+
+     ;;; Code:
+
+     (evil-leader/set-key-for-mode 'java-mode "t"
+       (lambda ()
+         (interactive)
+         (meghanada-run-task "test")
+         (ace-window "")
+         (golden-ratio)
+         )
+       )
+     (evil-leader/set-key-for-mode 'java-mode "c"
+       (lambda ()
+         (interactive)
+         (meghanada-run-task "run")
+         (ace-window "")
+         (golden-ratio)
+         )
+       )
+
+
+     (add-hook 'java-mode-hook
+   	    (lambda ()
+   	      ;; meghanada-mode on
+   	      (meghanada-mode t)
+   	      (add-hook 'before-save-hook (lambda ()
+   					    (flycheck-display-error-at-point)
+   					    (meghanada-code-beautify-before-save)
+   					    ))))
+
+
+   #+END_SRC

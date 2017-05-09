7e8f880541a7019c46507663709ec89d227b2d86
cescoferraro <francescoaferraro@gmail.com>
2017-05-09 08:58:49 -0300

    hydring

--- src/emacs.d/custom.el
+++ src/emacs.d/custom.el
@@ -3,13 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-    '(custom-safe-themes
-         (quote
-             ("f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
+ '(custom-safe-themes
+   (quote
+    ("f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
  '(inhibit-startup-screen t)
-    '(package-selected-packages
-         (quote
-             (lispy telephone-line spaceline-all-the-icons powerline-evil emoji-cheat-sheet-plus gore-mode gorepl-mode js2-refactor js2-mode ac-js2 rainbow-delimiters rainbow-mode git-gutter-fringe git-gutter-fringe+ git-gutter+ gradle-mode openwith org-pdfview pdf-tools company-emacs-eclim eclim interleave w3m find-file-in-project helm-ag tabbar-ruler tabbar magit-gitflow company-go expand-line evil-nerd-commenter evil-commentary helm-dash timonier evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window)))
+ '(package-selected-packages
+   (quote
+    (origami fold-dwim groovy-imports move-text lispy telephone-line spaceline-all-the-icons powerline-evil emoji-cheat-sheet-plus gore-mode gorepl-mode js2-refactor js2-mode ac-js2 rainbow-delimiters rainbow-mode git-gutter-fringe git-gutter-fringe+ git-gutter+ gradle-mode openwith org-pdfview pdf-tools company-emacs-eclim eclim interleave w3m find-file-in-project helm-ag tabbar-ruler tabbar magit-gitflow company-go expand-line evil-nerd-commenter evil-commentary helm-dash timonier evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window)))
  '(tabbar-separator (quote (0.5))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

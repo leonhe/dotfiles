39cab35be6c58ce46e052c69f2c330ed93034583
cescoferraro <francescoaferraro@gmail.com>
2018-08-28 17:00:26 -0300

    remote dired and emacswiki stuff

--- src/emacs.d/custom.el
+++ src/emacs.d/custom.el
@@ -3,19 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-    '(custom-safe-themes
-         (quote
-             ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "fad38808e844f1423c68a1888db75adf6586390f5295a03823fa1f4959046f81" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "fad38808e844f1423c68a1888db75adf6586390f5295a03823fa1f4959046f81" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
  '(inhibit-startup-screen t)
-    '(package-selected-packages
-         (quote
-             (ssh go-snippets diminish vue-mode evil-mc prettier-js js3-mode rinari textmate ox-pandoc org-agenda org-ac noflet org-gcal org-download dired-rainbow dired-details+ eyebrowse fill-column-indicator smooth-scrolling project-explorer company-emoji ox-latex darcula-theme spaceline-config spacemacs-theme deferred company-edbi javadoc-lookup flyspell-popup discover-clj-refactor cljr-helm clj-refactor inf-clojure dockerfile-mode sqlplus helm-ag-r easy-hugo skewer-mode cider ranger dired-ranger ag dired-atool dired dired-quick-sort dired+ all-the-icons-dired password-generator toggle-quotes helm-unicode org-present camcorder golden-ratio-scroll-screen anaconda-mode free-keys evil-vimish-fold origami fold-dwim groovy-imports move-text lispy telephone-line spaceline-all-the-icons powerline-evil emoji-cheat-sheet-plus gore-mode gorepl-mode js2-refactor js2-mode ac-js2 rainbow-delimiters rainbow-mode git-gutter-fringe git-gutter-fringe+ git-gutter+ gradle-mode openwith org-pdfview pdf-tools company-emacs-eclim eclim interleave w3m find-file-in-project helm-ag tabbar-ruler tabbar magit-gitflow company-go expand-line evil-nerd-commenter evil-commentary helm-dash timonier evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window)))
+ '(package-selected-packages
+   (quote
+    (ssh go-snippets diminish vue-mode evil-mc prettier-js js3-mode rinari textmate ox-pandoc org-agenda org-ac noflet org-gcal org-download dired-rainbow dired-details+ eyebrowse fill-column-indicator smooth-scrolling project-explorer company-emoji ox-latex darcula-theme spaceline-config spacemacs-theme deferred company-edbi javadoc-lookup flyspell-popup discover-clj-refactor cljr-helm clj-refactor inf-clojure dockerfile-mode sqlplus helm-ag-r easy-hugo skewer-mode cider ranger dired-ranger ag dired-atool dired dired-quick-sort dired+ all-the-icons-dired password-generator toggle-quotes helm-unicode org-present camcorder golden-ratio-scroll-screen anaconda-mode free-keys evil-vimish-fold origami fold-dwim groovy-imports move-text lispy telephone-line spaceline-all-the-icons powerline-evil emoji-cheat-sheet-plus gore-mode gorepl-mode js2-refactor js2-mode ac-js2 rainbow-delimiters rainbow-mode git-gutter-fringe git-gutter-fringe+ git-gutter+ gradle-mode openwith org-pdfview pdf-tools company-emacs-eclim eclim interleave w3m find-file-in-project helm-ag tabbar-ruler tabbar magit-gitflow company-go expand-line evil-nerd-commenter evil-commentary helm-dash timonier evil-smartparens engine-mode aggressive-indent smart-mode-line-powerline-theme beacon zenburn-theme ample-zen-theme iedit color-theme ox-reveal emojify spotify twittering-mode dashboard simpleclip all-the-icons yaml-mode ergoemacs-mode docker hl-line+ which-key zoom-window web-mode-edit-element vimrc-mode use-package tide spaceline smex smartparens prodigy popwin pallet org-plus-contrib org-bullets ob-typescript ob-go nyan-mode neotree multiple-cursors meghanada markdown-mode linum-relative idle-highlight-mode htmlize helm-projectile groovy-mode golint golden-ratio godoctor go-guru go-eldoc go-autocomplete flycheck-cask expand-region exec-path-from-shell evil-surround evil-org evil-magit esup emmet-mode editorconfig drag-stuff counsel ace-window)))
  '(prettier-js-args (quote ("--single-quotes")))
-    '(safe-local-variable-values
-         (quote
-             ((cider-cljs-lein-repl . "(do (user/run) (user/browser-repl))")
-                 (cider-refresh-after-fn . "reloaded.repl/resume")
-                 (cider-refresh-before-fn . "reloaded.repl/suspend"))))
+ '(safe-local-variable-values
+   (quote
+    ((cider-cljs-lein-repl . "(do (user/run) (user/browser-repl))")
+     (cider-refresh-after-fn . "reloaded.repl/resume")
+     (cider-refresh-before-fn . "reloaded.repl/suspend"))))
  '(tabbar-separator (quote (0.5))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
